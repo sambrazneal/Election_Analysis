473bd0864308ed25f130000fe8a397250dda99b2
sambrazneal <103452159+sambrazneal@users.noreply.github.com>
2022-05-14 22:46:02 -0500

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -7,8 +7,6 @@
 
 import csv 
 import os
-from wsgiref import headers 
-
 
 # Assign a variable for a file to load and the path. 
 file_to_load = '/Users/samanthaneal/Documents/Election_Analysis/Resources/election_results .csv'
@@ -16,21 +14,70 @@ file_to_load = '/Users/samanthaneal/Documents/Election_Analysis/Resources/electi
 # Assign a varible to save the file to a path 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter. 
+total_votes = 0 
+
+# Canidate options and canidate votes 
+canidate_options = []
+
+# Declare the empty dictionary 
+canidate_votes = {}
+
+# Winning Canidate and Winning Count Tracker 
+winning_canidate = ""
+winning_count = 0 
+winning_percentage = 0 
+
 # Open the election results and read the file. 
 with open(file_to_load) as election_data:
-
-     # To do: read and analyze the data here.
-
-     # Read the file object with the reader function 
      file_reader = csv.reader(election_data)
      
-     # Print each row in the CSV file. 
+     # Read the header row 
      headers = next(file_reader)
-     print(headers)
 
+     # Print each row in the CSV file. 
+     for row in file_reader:
+          # Add to total vote count
+          total_votes += 1
 
-# Using the with statement open file as text 
-with open(file_to_save, "w") as txt_file:
+          # Print the canidate name from each row
+          canidate_name = row[2]
 
-# Write some data to file 
-     txt_file.write("Arapahoe\nDenver\nJefferson")
+          # If the canidate does not match any existing canidate... 
+          if canidate_name not in canidate_options:
+               # Add it to the list of canidates
+               canidate_options.append(canidate_name)
+
+               # Begin tracking that canidate's vote count 
+               canidate_votes[canidate_name] = 0 
+
+          # Add a vote to that canidate's count 
+          canidate_votes[canidate_name] += 1 
+
+# Determine the percentage of votes for each canidate by looping through the counts
+# Iterate through the canidate list 
+for canidate_name in canidate_votes:
+     # Retrieve vote count of a canidate 
+     votes = canidate_votes[canidate_name]
+     #Calculate the percentage of votes 
+     vote_percentage = float(votes) / float(total_votes) * 100 
+     # Print out each canidate's name, vote count, and percentage of votes to the terminal 
+     print(f"{canidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+# Determine winning vote and canidate 
+# Determine if the votes are greater than the winning count 
+     if (votes > winning_count) and (vote_percentage > winning_percentage):
+          # if true then set winning_count = votes and winning percent = vote_percentage 
+          winning_count = votes
+          winning_percentage = vote_percentage
+          # Set the winning_canidate equal to canidate's name
+          winning_canidate = canidate_name
+
+winning_canidate_summary = (
+     f"--------------------------\n"
+     f"Winner: {winning_canidate}\n"
+     f"Winning Vote Count: {winning_count:,}\n"
+     f"Winning Percentage: {winning_percentage:.1f}%\n"
+     f"--------------------------\n")
+
+print(winning_canidate_summary)
